4f732ce1628a54103a89fe02f5b916e87cd7d3e2
Iker Vélez <168359086+Iker1211@users.noreply.github.com>
2025-01-21 00:51:02 +0000

    Add buy me a coffe widget

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 
 const hour = document.querySelectorAll(".hour");
 let array = Array.from(hour);
-
+console.log(array.length);
 let current_index = 0;
 let count = 3600;
-
-let alarm = new Audio('assets/sounds/funny-alarm.mp3');
-alarm.play();
 
 function change_color(element) {
     element.style.backgroundColor = 'var(--gray)';
 }
 
 function start_timer() {
+  let alarm = new Audio('assets/sounds/alarm.mp3');
+
+  let go_to_sleep = new Audio('assets/sounds/sleep-spell.mp3')
 
   const timer = setInterval(function() {
     count--;
     console.log(count); //see
 
-    if (count === 0) {
+    if (count === 0 && current_index < array.length) {
       change_color(array[current_index]);
-
+      alarm.play();
       current_index++;
 
       count = 3600;
   
       if (current_index >= array.length) {
         clearInterval(timer);
-        alert('Duerma mamita');
+        go_to_sleep.play();
       }
-    }
+    } 
   }, 1000);
 }
 
