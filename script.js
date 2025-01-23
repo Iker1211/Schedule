db9bacdc479fb71837ff83b325275013e66cd5f2
Iker Vélez <168359086+Iker1211@users.noreply.github.com>
2025-01-23 07:14:19 +0000

    Add DOM content via JavaScript to see the counter

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+const btn_iniciar = document.querySelector(".button-2");
+const container = document.querySelector(".container");
 const btn = document.querySelectorAll(".button-3");
 const hour = document.querySelectorAll(".hour");
 
-let toggle = false;
+let counter = document.createElement('b');
+counter.style.fontSize = '2rem';
+counter.style.margin = '0 0 0 2rem';
+
 let array = Array.from(hour);
 let current_index = 0;
 let count = 3600;
+let toggle = false;
+let cuenta_atras = false;
 
 function change_color(element) {
     element.style.backgroundColor = 'var(--gray)';
@@ -16,18 +23,51 @@ btn.forEach( element => {
   });
 });
 
+btn_iniciar.onclick = () => {
+
+  /* variable_contador += variable_contador; */ 
+  /* Luego if compruebo */
+
+ if (toggle) {
+  cuenta_atras = !cuenta_atras;
+ }
+ 
+ cuenta_atras = !cuenta_atras;
+ 
+    toggle = false;
+}
+
+let pause = false;
+
 function start_timer() {
 
   let alarm = new Audio('assets/sounds/alarm.mp3');
   let go_to_sleep = new Audio('assets/sounds/sleep-spell.mp3')
 
   const timer = setInterval(function() {
-    if (!toggle) {
-      count--;
-    } else {
-      count++;
+
+    if (cuenta_atras) {
+
+      if (toggle) {
+        btn_iniciar.style.display = 'inline-block';
+        btn_iniciar.textContent = 'Continuar Sesión';
+
+        if (btn_iniciar.textContent === 'Continuar Sesión') {
+
+          // nested
+           if (toggle) {
+            pause = true;
+            if (pause) {
+              btn_iniciar.textContent = 'Pausar Sesión';
+            }
+           }
+        }
+
+        count++;
+      } else {
+        count--;
+      }
     }
-    console.log(count); //see
 
     if (count === 0 && current_index < array.length) {
       change_color(array[current_index]);
@@ -41,6 +81,9 @@ function start_timer() {
         go_to_sleep.play();
       }
     } 
+
+    container.appendChild(counter).textContent = count;
+
   }, 1000);
 }
 
